6e4752359491b36cd6fe01e346982dba1e635abb
Zohaib Nasir <zohaibnasirmughal@gmail.com>
2021-02-24 16:32:41 +0500

    migration changes made by hassan493 on my PC

--- api/migrations/02_preferences.js
+++ api/migrations/02_preferences.js
@@ -1,24 +1,30 @@
 /* jshint indent: 1 */
 
 module.exports = {
-    up: (queryInterface, Sequelize) => queryInterface.createTable('preferences', {
-		id: {
-			type: Sequelize.DataTypes.INTEGER(11),
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		option_name: {
-			type: Sequelize.DataTypes.STRING(255),
-			allowNull: false
-		},
-		option_value: {
-			type: Sequelize.DataTypes.STRING(255),
-			allowNull: false
-		}
-	}, {
-		tableName: 'preferences',
-		}).then(() =>  {
-	}),
-	down: (queryInterface) => queryInterface.dropTable('preferences'),
+  up: (queryInterface, Sequelize) =>
+    queryInterface
+      .createTable(
+        "preferences",
+        {
+          id: {
+            type: Sequelize.DataTypes.INTEGER(11),
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          option_name: {
+            type: Sequelize.DataTypes.STRING(255),
+            allowNull: false,
+          },
+          option_value: {
+            type: Sequelize.DataTypes.STRING(255),
+            allowNull: false,
+          },
+        },
+        {
+          tableName: "preferences",
+        }
+      )
+      .then(() => {}),
+  down: (queryInterface) => queryInterface.dropTable("preferences"),
 };
