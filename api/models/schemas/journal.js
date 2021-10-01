fbc383d89ed490b3c951794b1f72ddd1604c00d9
Hassan Yousaf <hassanyousaf565@gmail.com>
2021-10-02 04:00:26 +0500

    crud completed for journal

---
+++ api/models/schemas/journal.js
@@ -0,0 +1,98 @@
+/* jshint indent: 1 */
+
+module.exports = (sequelize, DataTypes) => sequelize.define('journal', {
+    id: {
+        type: DataTypes.INTEGER(11),
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    user_id: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    },
+    subject: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+    },
+    content_id: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true,
+        references: {
+            model: 'content',
+            key: 'id'
+        }
+    },
+    start_date: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    end_date: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    track_my_goal: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+    log_date: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    points: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true,
+        defaultValue: 0
+    },
+    status: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+    },
+    type: {
+        type: DataTypes.ENUM(
+            'goal',
+            'journey',
+            'journal'
+        )
+    },
+    parent: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+    },
+
+    created_by: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    updated_by: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    },
+}, {
+    tableName: 'journal',
+    timestamps: false,
+});
+module.exports.initRelations = () => {
+    delete module.exports.initRelations; // Destroy itself to prevent repeated calls.
+};
