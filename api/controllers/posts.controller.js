3d92684a3929b7b964ea5039352c9081b8cf07c8
Hassan Yousaf <hassanyousaf565@gmail.com>
2022-03-15 23:04:13 +0500

    photo added to comments and get posts api

--- api/controllers/posts.controller.js
+++ api/controllers/posts.controller.js
@@ -97,29 +97,53 @@ async function findTimelinePosts(userId, req) {
             ['id', 'DESC'],
             ['publish_date', 'DESC']
         ],
-        raw:true,
+        attributes: [
+            'id',
+            'user_id',
+            'timeline_id',
+            'group_id',
+            'title',
+            'content',
+            'assigned_group',
+            'status',
+            'feeling',
+            'media',
+            'love_count',
+            'comment_count',
+            'share_count',
+            'publish_date',
+            'created_at',
+            'updated_at',
+            'deleted_at',
+            'loved_by'
+        ],
+        raw: true,
+        include: [{ model: model.users, attributes: ['first_name', 'last_name' , 'photo_url'] }],
         ...req.pagination
 
     })
 
-    for (post of posts){
+    for (post of posts) {
         let comments = await commentService.findAndCount({ where: { post_id: post.id, parent_id: null, deleted_at: null }, raw: true })
         post.comment_count = comments.count;
     }
-   
+
 
     return posts;
 }
 
-async function deletePostByID(postId, userId) {
+async function deletePostByID(postId, user) {
     let deletedAt = new Date();
-    let postDb ; 
+    let postDb;
+    let userId = user.id;
+
     if (user.role == 'content-manager' || user.role == 'system-administrator') {
         postDb = await postService.update({ deletedAt }, { where: { id: postId } });
-    }else{
+    } else {
         postDb = await postService.update({ deletedAt }, { where: { id: postId, user_id: userId } });
     }
-    
+
+
     return postDb;
 }
 
@@ -182,6 +206,7 @@ async function deletepost(req, res) {
     //     user_id:user.id,
     //     id:postId
     // }})
+
     deletePostByID(postId, user)
         .then(result => {
 
@@ -239,11 +264,11 @@ async function givelove(req, res) {
         post_loved_by = JSON.stringify(prev_loves)
     }
 
-    let updateResp = await postService.update({love_count , loved_by:post_loved_by}, { where: { id } })
-    if(updateResp[0] == 1){
-        res.send({love_count})
-    }else{
-        res.status(400).send({message:"an error occured" , updateResp})
+    let updateResp = await postService.update({ love_count, loved_by: post_loved_by }, { where: { id } })
+    if (updateResp[0] == 1) {
+        res.send({ love_count })
+    } else {
+        res.status(400).send({ message: "an error occured", updateResp })
     }
-    
+
 }
